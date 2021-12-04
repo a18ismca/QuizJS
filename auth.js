63a8f149a0930b58e9df362546754e10792d5d02
a18ismca <a18ismca@student.his.se>
2021-12-04 14:36:17 +0100

    Rechange all!

--- auth.js
+++ auth.js
@@ -5,14 +5,11 @@ function isPasswordCorrect(){
     var getPassword = document.getElementById("pwd").value;
 
     if(getUsername === "a18ismca" && getPassword === "Syp9393"){
-        document.getElementById("statement").innerHTML = "Redirecting you in a few seconds..."
+        document.getElementById("statement").innerHTML = "Redirecting you to quiz menu in a few seconds..."
         location.replace("menu.html")
     } else {
         document.getElementById("statement").innerHTML = "The username or the password is incorrect. Try again."
     }
 }
 
-function redirectToPassatQuiz(){
-    location.replace("q1passat.html");
-}
 
