9ca777a7f92338a9f0989b94d1178cb3a13b4fb8
a18ismca <a18ismca@student.his.se>
2021-12-09 19:39:56 +0100

    New!

--- auth.js
+++ auth.js
@@ -1,15 +1,80 @@
 
 
-function isPasswordCorrect(){
-    var getUsername = document.getElementById("login").value;
-    var getPassword = document.getElementById("pwd").value;
+var accounts = [
+    {
+        username: "a18ismca",
+        password: "Syp9393",
+    },
+    {
+        username: "root",
+        password: "root",
+    },
+    {
+        username: "iscat",
+        password: "iscat",
+    },
+    {
+        username: "admin",
+        password: "password",
+    },
+    {
+        username: "windows",
+        password: "xp2001",
+    }
+];
 
-    if(getUsername === "a18ismca" && getPassword === "Syp9393"){
-        document.getElementById("statement").innerHTML = "Redirecting you to quiz menu in a few seconds..."
-        location.replace("menu.html")
-    } else {
-        document.getElementById("statement").innerHTML = "The username or the password is incorrect. Try again."
+
+function loginAttempt(){
+    var loginAttempts = 3;
+
+    var getUsername = document.getElementById("username").value;
+    var getPassword = document.getElementById("pwd").value;
+    var i;
+    while(loginAttempts > 0){
+    for(i = 0; i < accounts.length; i++){
+    if(getUsername === accounts[i].username && getPassword === accounts[i].password){
+      enterMenu();
+    }
+} if(loginAttempts == 1){
+         return noAttemptsLeft();
+        }
+        else{
+            loginAttempts--;
+        }
+        document.getElementById("statement").innerHTML = "Wrong username or password! Login attempts: " + loginAttempts;
+        document.getElementById("statement").style.color = "red";
+
     }
 }
 
 
+
+
+function enterMenu(){
+    document.getElementById("statement").innerHTML = "Redirecting you to the quiz menu..."
+    document.getElementById("statement").style.color = "green";
+    location.replace("menu.html");
+}
+
+function noAttemptsLeft(){
+    document.getElementById("statement").innerHTML = "No more attempts left to login!";
+    removeObjectsOnScreen();
+    var count = 59;
+    var timer = setInterval(function(){
+        if(count <= 0){
+            clearInterval(timer);
+            location.replace("login.html");
+        }
+        document.getElementById("statement").innerHTML = "No more attempts left to login! Retry in " + count + " seconds... ";
+        count -=1;
+    }, 1000);
+    removeObjectsOnScreen();
+
+}
+
+
+
+function removeObjectsOnScreen(){
+    document.getElementById("not").remove();
+    document.getElementById("header").remove();
+}
